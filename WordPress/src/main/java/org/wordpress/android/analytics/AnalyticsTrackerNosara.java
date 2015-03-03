96ef360480f4b8906785623e12ed01e7e79f7cf6
Danilo Ercoli <ercoli@gmail.com>
2015-03-03 16:39:45 +0100

    Moving the bar around.

--- WordPress/src/main/java/org/wordpress/android/analytics/AnalyticsTrackerNosara.java
+++ WordPress/src/main/java/org/wordpress/android/analytics/AnalyticsTrackerNosara.java
@@ -1,52 +1,42 @@
 package org.wordpress.android.analytics;
 
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.util.DisplayMetrics;
 import android.util.Log;
 
-import com.android.volley.NetworkResponse;
-import com.android.volley.RequestQueue;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.Volley;
-import com.wordpress.rest.RestRequest;
-
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.Map;
 
 public class AnalyticsTrackerNosara implements AnalyticsTracker.Tracker {
-    public static final String TAG = "AnalyticsTrackerNosara";
+    public static final String LOGTAG = "AnalyticsTrackerNosara";
 
-    private RequestQueue mRequestQueue;
+
     private NosaraRestClient mRestClient;
 
     public AnalyticsTrackerNosara(Context ctx) {
-        mRequestQueue = Volley.newRequestQueue(ctx);
-        mRestClient = new NosaraRestClient(mRequestQueue);
+        if (null == ctx || !checkBasicConfiguration(ctx)) {
+            mRestClient = null;
+            return;
+        }
+        mRestClient = new NosaraRestClient(ctx);
     }
 
-    private class RestListener implements RestRequest.Listener, RestRequest.ErrorListener {
-        @Override
-        public void onResponse(final JSONObject response) {
-            Log.d(TAG, response.toString());
+
+    private static boolean checkBasicConfiguration(Context context) {
+        final PackageManager packageManager = context.getPackageManager();
+        final String packageName = context.getPackageName();
+
+        if (PackageManager.PERMISSION_GRANTED != packageManager.checkPermission("android.permission.INTERNET", packageName)) {
+            Log.w(LOGTAG, "Package does not have permission android.permission.INTERNET - Nosara Client will not work at all!");
+            Log.i(LOGTAG, "You can fix this by adding the following to your AndroidManifest.xml file:\n" +
+                    "<uses-permission android:name=\"android.permission.INTERNET\" />");
+            return false;
         }
 
-        @Override
-        public void onErrorResponse(final VolleyError volleyError) {
-            if (volleyError == null) {
-                Log.e(TAG, "Tried to log a VolleyError, but the error obj was null!");
-                return;
-            }
-            if (volleyError.networkResponse != null) {
-                NetworkResponse networkResponse = volleyError.networkResponse;
-                Log.e(TAG, "Network status code: " + networkResponse.statusCode);
-                if (networkResponse.data != null) {
-                    Log.e(TAG, "Network data: " + new String(networkResponse.data));
-                }
-            }
-            Log.e(TAG, "Volley Error details: " + volleyError.getMessage(), volleyError);
-        }
+        return true;
     }
 
 
@@ -57,86 +47,75 @@ public class AnalyticsTrackerNosara implements AnalyticsTracker.Tracker {
 
     @Override
     public void track(AnalyticsTracker.Stat stat, Map<String, ?> properties) {
-
-        JSONObject params = new JSONObject();
+        if (mRestClient == null) {
+            return;
+        }
 
         String eventName;
 
         switch (stat) {
             case APPLICATION_STARTED:
-                eventName = "Application Started";
-                //instructions.setSuperPropertyToIncrement("Application Started");
+                eventName = "application_started";
                 break;
             case APPLICATION_OPENED:
-                eventName = "Application Opened";
-                //instructions.setSuperPropertyToIncrement("Application Opened");
-                //incrementSessionCount();
+                eventName = "application_opened";
                 break;
             case APPLICATION_CLOSED:
-                eventName = "Application Closed";
+                eventName = "application_closed";
                 break;
             case READER_ACCESSED:
-                eventName = "Reader - Accessed";
+                eventName = "reader_accessed";
                 //instructions.setSuperPropertyAndPeoplePropertyToIncrement("number_of_times_accessed_reader");
                 //instructions.setCurrentDateForPeopleProperty("last_time_accessed_reader");
                 break;
             case READER_OPENED_ARTICLE:
-                eventName = "Reader - Opened Article";
+                eventName = "reader_opened_article";
                 //instructions.setSuperPropertyAndPeoplePropertyToIncrement("number_of_times_opened_article");
                 //instructions.setCurrentDateForPeopleProperty("last_time_opened_reader_article");
                 break;
             case READER_LIKED_ARTICLE:
-                eventName = "Reader - Liked Article";
+                eventName = "reader_liked_article";
                 //instructions.setSuperPropertyAndPeoplePropertyToIncrement("number_of_times_liked_article");
                 //instructions.setCurrentDateForPeopleProperty("last_time_liked_reader_article");
                 break;
-
             case READER_INFINITE_SCROLL:
-                eventName = "Reader - Infinite Scroll";
+                eventName = "reader_infinite_scroll_performed";
                 //instructions.setSuperPropertyAndPeoplePropertyToIncrement(
                 //      "number_of_times_reader_performed_infinite_scroll");
                 //instructions.setCurrentDateForPeopleProperty("last_time_performed_reader_infinite_scroll");
                 break;
-
             case NOTIFICATIONS_ACCESSED:
-                eventName = "Notifications - Accessed";
+                eventName = "notifications_accessed";
                 //instructions.setSuperPropertyAndPeoplePropertyToIncrement("number_of_times_accessed_notifications");
                 //instructions.setCurrentDateForPeopleProperty("last_time_accessed_notifications");
                 break;
-            case NOTIFICATIONS_OPENED_NOTIFICATION_DETAILS:
-                eventName = "Notifications - Opened Notification Details";
-                // instructions.
-                //        setSuperPropertyAndPeoplePropertyToIncrement("number_of_times_opened_notification_details");
-                //instructions.setCurrentDateForPeopleProperty("last_time_opened_notification_details");
-                break;
-
             case STATS_ACCESSED:
-                eventName = "Stats - Accessed";
+                eventName = "stats_accessed";
                 //instructions.setSuperPropertyAndPeoplePropertyToIncrement("number_of_times_accessed_stats");
                 //instructions.setCurrentDateForPeopleProperty("last_time_accessed_stats");
                 break;
             case STATS_VIEW_ALL_ACCESSED:
-                eventName = "Stats - View All Accessed";
+                eventName = "stats_view_all_accessed";
                 //instructions.setSuperPropertyAndPeoplePropertyToIncrement("number_of_times_accessed_view_all_screen_stats");
                 //instructions.setCurrentDateForPeopleProperty("last_time_accessed_view_all_screen_stats");
                 break;
             case STATS_SINGLE_POST_ACCESSED:
-                eventName = "Stats - Single Post Accessed";
+                eventName = "stats_single_post_accessed";
                 //instructions.setSuperPropertyAndPeoplePropertyToIncrement("number_of_times_accessed_single_post_screen_stats");
                 //instructions.setCurrentDateForPeopleProperty("last_time_accessed_single_post_screen_stats");
                 break;
             case STATS_OPENED_WEB_VERSION:
-                eventName = "Stats - Opened Web Version";
+                eventName = "stats_web_version_accessed";
                 //instructions.setSuperPropertyAndPeoplePropertyToIncrement("number_of_times_accessed_web_version_of_stats");
                 //instructions.setCurrentDateForPeopleProperty("last_time_accessed_web_version_of_stats");
                 break;
             case STATS_TAPPED_BAR_CHART:
-                eventName = "Stats - Tapped Bar Chart";
+                eventName = "stats_tapped_bar_chart";
                 //instructions.setSuperPropertyAndPeoplePropertyToIncrement("number_of_times_tapped_stats_bar_chart");
                 //instructions.setCurrentDateForPeopleProperty("last_time_tapped_stats_bar_chart");
                 break;
             case STATS_SCROLLED_TO_BOTTOM:
-                eventName = "Stats - Scrolled to Bottom";
+                eventName = "stats_scrolled_to_bottom";
                 //instructions.setSuperPropertyAndPeoplePropertyToIncrement("number_of_times_scrolled_to_bottom_of_stats");
                 //instructions.setCurrentDateForPeopleProperty("last_time_scrolled_to_bottom_of_stats");
                 break;
@@ -154,13 +133,7 @@ public class AnalyticsTrackerNosara implements AnalyticsTracker.Tracker {
             JSONObject singleEventParams = new JSONObject();
             singleEventParams.put("_en", eventName);
             singleEventParams.put("_ui", "16154691");
-
-            JSONArray events = new JSONArray();
-            events.put(singleEventParams);
-
-            params.put("events", events);
-            mRestClient.track(params, new RestListener(), new RestListener());
-
+            mRestClient.track(singleEventParams);
         } catch (JSONException e) {
             e.printStackTrace();
         }
@@ -170,26 +143,34 @@ public class AnalyticsTrackerNosara implements AnalyticsTracker.Tracker {
 
     @Override
     public void beginSession() {
-
+        if (mRestClient == null) {
+            return;
+        }
     }
 
     @Override
     public void endSession() {
-
+        if (mRestClient == null) {
+            return;
+        }
     }
 
     @Override
     public void refreshMetadata() {
-
+        if (mRestClient == null) {
+            return;
+        }
     }
 
     @Override
     public void clearAllData() {
-
+        if (mRestClient == null) {
+            return;
+        }
     }
 
     @Override
     public void registerPushNotificationToken(String regId) {
-
+        return;
     }
 }
--- WordPress/src/main/java/org/wordpress/android/analytics/NosaraRestClient.java
+++ WordPress/src/main/java/org/wordpress/android/analytics/NosaraRestClient.java
@@ -1,19 +1,38 @@
 package org.wordpress.android.analytics;
 
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.util.DisplayMetrics;
+import android.util.Log;
+
 import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.NetworkResponse;
 import com.android.volley.Request.Method;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
 import com.android.volley.Response.ErrorListener;
 import com.android.volley.Response.Listener;
 import com.android.volley.RetryPolicy;
+import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
-public class NosaraRestClient {
-    public static final String TAG = "NosaraRestClient";
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
-    private static String DEFAULT_USER_AGENT = "WordPress Nosara Client for Android";
+public class NosaraRestClient {
+    public static final String LOGTAG = "NosaraRestClient";
+
+    public static final String LIB_VERSION = "0.0.1";
+    protected static final String DEFAULT_USER_AGENT = "Nosara Client for Android";
+    protected static final String NOSARA_REST_API_ENDPOINT_URL_V1_1 = "https://public-api.wordpress.com/rest/v1.1/";
 
     /**
      * Socket timeout in milliseconds for rest requests
@@ -35,39 +54,76 @@ public class NosaraRestClient {
      */
     public static final float REST_BACKOFF_MULT = 2f;
 
-
-    protected static final String NOSARA_REST_API_ENDPOINT_URL_V1_1 = "https://public-api.wordpress.com/rest/v1.1/";
     private String mUserAgent = NosaraRestClient.DEFAULT_USER_AGENT;
-    private RequestQueue mQueue;
+    private final RequestQueue mQueue;
     private String mRestApiEndpointURL;
+    private final Map<String, String> mDeviceInfo;
+    private final NosaraExtendedSystemInformation extendedSystemInformation;
 
-    public NosaraRestClient(RequestQueue queue) {
-        mQueue = queue;
+
+    public NosaraRestClient(Context ctx) {
+        mQueue = Volley.newRequestQueue(ctx);
         mRestApiEndpointURL = NOSARA_REST_API_ENDPOINT_URL_V1_1;
+
+        final Map<String, String> deviceInfo = new HashMap<String, String>();
+        deviceInfo.put("$android_lib_version", LIB_VERSION);
+        deviceInfo.put("$android_os", "Android");
+        deviceInfo.put("$android_os_version", Build.VERSION.RELEASE == null ? "UNKNOWN" : Build.VERSION.RELEASE);
+        deviceInfo.put("$android_manufacturer", Build.MANUFACTURER == null ? "UNKNOWN" : Build.MANUFACTURER);
+        deviceInfo.put("$android_brand", Build.BRAND == null ? "UNKNOWN" : Build.BRAND);
+        deviceInfo.put("$android_model", Build.MODEL == null ? "UNKNOWN" : Build.MODEL);
+        try {
+            final PackageManager manager = ctx.getPackageManager();
+            final PackageInfo info = manager.getPackageInfo(ctx.getPackageName(), 0);
+            deviceInfo.put("$android_app_version", info.versionName);
+            deviceInfo.put("$android_app_version_code", Integer.toString(info.versionCode));
+        } catch (final PackageManager.NameNotFoundException e) {
+            Log.e(LOGTAG, "Exception getting app version name", e);
+        }
+        mDeviceInfo = Collections.unmodifiableMap(deviceInfo);
+        extendedSystemInformation = new NosaraExtendedSystemInformation(ctx);
     }
 
-    public NosaraRestClient(RequestQueue queue, String endpointURL) {
-        this(queue);
+    public NosaraRestClient(Context ctx, String endpointURL) {
+        this(ctx);
         mRestApiEndpointURL = endpointURL;
     }
 
     // Volley send the request
-    public JsonObjectRequest track(JSONObject jsonRequest, Listener<JSONObject> listener, ErrorListener errorListener) {
+    public JsonObjectRequest track(JSONObject singleEvent) {
+       //FIXME: Send the request to server. One request per connection for now. No BATCHING
+        JSONObject requestJSONObject = new JSONObject();
+       try {
+           singleEvent.put("_via_ua", getUserAgent());
+           JSONArray events = new JSONArray();
+           events.put(singleEvent);
+           requestJSONObject.put("events", events);
+
+           JSONObject commonProps = new JSONObject();
+           commonProps.put("device_info", new JSONObject(mDeviceInfo));
+           commonProps.put("extended_device_info", getExtendedSystemInfo());
+
+           requestJSONObject.put("commonProps", commonProps);
+       } catch (JSONException err) {
+           Log.e(LOGTAG, "Exception creating the request JSON object", err);
+           return null;
+       }
         String path = "tracks/record";
-        NosaraRestRequest request = this.post(path, jsonRequest, listener, errorListener);
+        NosaraRestListener nosaraRestListener = new NosaraRestListener(requestJSONObject);
+        NosaraRestRequest request = this.post(path, requestJSONObject, nosaraRestListener, nosaraRestListener);
         mQueue.add(request);
         return request;
     }
 
-    public NosaraRestRequest get(String path, Listener<JSONObject> listener, ErrorListener errorListener) {
+    private NosaraRestRequest get(String path, Listener<JSONObject> listener, ErrorListener errorListener) {
         return makeRequest(Method.GET, getAbsoluteURL(path), null, listener, errorListener);
     }
 
-    public NosaraRestRequest post(String path, JSONObject jsonRequest, Listener<JSONObject> listener, ErrorListener errorListener) {
+    private NosaraRestRequest post(String path, JSONObject jsonRequest, Listener<JSONObject> listener, ErrorListener errorListener) {
         return this.post(path, jsonRequest, null, listener, errorListener);
     }
 
-    public NosaraRestRequest post(final String path, JSONObject jsonRequest, RetryPolicy retryPolicy, Listener<JSONObject> listener, ErrorListener errorListener) {
+    private NosaraRestRequest post(final String path, JSONObject jsonRequest, RetryPolicy retryPolicy, Listener<JSONObject> listener, ErrorListener errorListener) {
         final NosaraRestRequest request = makeRequest(Method.POST, getAbsoluteURL(path), jsonRequest, listener, errorListener);
         if (retryPolicy == null) {
             retryPolicy = new DefaultRetryPolicy(REST_TIMEOUT_MS, REST_MAX_RETRIES_POST,
@@ -101,4 +157,84 @@ public class NosaraRestClient {
     public void setUserAgent(String userAgent) {
         mUserAgent = userAgent;
     }
+
+    public String getUserAgent() {
+        return mUserAgent;
+    }
+
+
+    private class NosaraRestListener implements Response.Listener<JSONObject>, Response.ErrorListener {
+        private final JSONObject mRequestObject; // Keep a reference to the opbj sent on the wire.
+
+        public NosaraRestListener(final JSONObject request) {
+            this.mRequestObject = request;
+        }
+
+        @Override
+        public void onResponse(final JSONObject response) {
+            Log.d(LOGTAG, response.toString());
+        }
+
+        @Override
+        public void onErrorResponse(final VolleyError volleyError) {
+            if (volleyError == null) {
+                Log.e(LOGTAG, "Tried to log a VolleyError, but the error obj was null!");
+                return;
+            }
+            if (volleyError.networkResponse != null) {
+                NetworkResponse networkResponse = volleyError.networkResponse;
+                Log.e(LOGTAG, "Network status code: " + networkResponse.statusCode);
+                if (networkResponse.data != null) {
+                    Log.e(LOGTAG, "Network data: " + new String(networkResponse.data));
+                }
+            }
+            Log.e(LOGTAG, "Volley Error details: " + volleyError.getMessage(), volleyError);
+        }
+    }
+
+
+    private JSONObject getExtendedSystemInfo() {
+        if (extendedSystemInformation == null) {
+            return null;
+        }
+
+        JSONObject extSysInfo = new JSONObject();
+        try {
+            extSysInfo.put("bluetooth_version", extendedSystemInformation.getBluetoothVersion());
+            extSysInfo.put("bluetooth_enabled", extendedSystemInformation.isBluetoothEnabled());
+        } catch (final JSONException e) {
+            Log.e(LOGTAG, "Exception writing bluetooth info values in JSON object", e);
+        }
+
+        try {
+            extSysInfo.put("current_network_operator", extendedSystemInformation.getCurrentNetworkOperator());
+            extSysInfo.put("phone_radio_type", extendedSystemInformation.getPhoneRadioType()); // NONE - GMS - CDMA - SIP
+            extSysInfo.put("wifi_connected", extendedSystemInformation.isWifiConnected());
+        } catch (final JSONException e) {
+            Log.e(LOGTAG, "Exception writing network info values in JSON object", e);
+        }
+
+        try {
+            extSysInfo.put("has_NFS", extendedSystemInformation.hasNFC());
+        } catch (final JSONException e) {
+            Log.e(LOGTAG, "Exception writing has_NFS value in JSON object", e);
+        }
+        try {
+            extSysInfo.put("has_telephony", extendedSystemInformation.hasTelephony());
+        } catch (final JSONException e) {
+            Log.e(LOGTAG, "Exception writing has_telephony value in JSON object", e);
+        }
+
+        try {
+            DisplayMetrics dMetrics = extendedSystemInformation.getDisplayMetrics();
+            extSysInfo.put("display_density_dpi", dMetrics.densityDpi);
+            extSysInfo.put("display_width_px", dMetrics.widthPixels);
+            extSysInfo.put("display_height_px", dMetrics.heightPixels);
+        } catch (final JSONException e) {
+            Log.e(LOGTAG, "Exception writing DisplayMetrics values in JSON object", e);
+        }
+
+        return extSysInfo;
+    }
+
 }
